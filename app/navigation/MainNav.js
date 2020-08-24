61fb214f47c33e812c938d30f8d5d73558768b34
kikusourabh <karmakarsourabh7@gmail.com>
2020-08-24 14:58:55 +0530

    asynch login and registeration done

--- app/navigation/MainNav.js
+++ app/navigation/MainNav.js
@@ -1,24 +1,44 @@
-import React from 'react';
-
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-
-import Login from '../screens/Login';
-import Register from '../screens/Register';
+import React, {useState, useEffect} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import {View} from 'react-native-animatable';
+import {ActivityIndicator} from 'react-native';
 import Home from '../screens/Home';
-
-const Stack = createStackNavigator();
+import AuthNav from './AuthNav';
 
 function MainNav() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator headerMode="none">
-        <Stack.Screen name="Login" component={Login}></Stack.Screen>
-        <Stack.Screen name="Registration" component={Register}></Stack.Screen>
-        <Stack.Screen name="Home" component={Home}></Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  const [isLogin, setIsLogin] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const check = async () => {
+      try {
+        const token = await AsyncStorage.getItem('token');
+        if (token !== null) {
+          setIsLogin(true);
+        } else {
+          setIsLogin(false);
+        }
+        setIsLoading(false);
+      } catch (e) {
+        console.log('Token store issue: ' + e);
+      }
+    };
+    check();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator />
+      </View>
+    );
+  } else {
+    if (isLogin) {
+      return <Home />;
+    } else {
+      return <AuthNav />;
+    }
+  }
 }
 
 export default MainNav;
