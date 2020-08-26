8641c7e7b1c1ffc0c6bc915add8b661e054ee89d
kikusourabh <karmakarsourabh7@gmail.com>
2020-08-26 16:34:07 +0530

    flat list-done

--- app/screens/QuoteListTab.js
+++ app/screens/QuoteListTab.js
@@ -1,16 +1,35 @@
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, View, Image} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
 import {Styles} from '../config/styles';
 import Axios from 'react-native-axios';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import {Colors} from '../config/colors';
-import {FlatList} from 'react-native-gesture-handler';
 
 function QuoteListTab() {
   const [data, setData] = useState([]);
+
+  const copyQuote = () => {};
   const QuotesItem = ({item}) => {
     return (
       <View style={styles.Item}>
-        <Image style={styles.ItemImg} source={{uri: item.img}} />
+        <View style={{flex: 3, flexDirection: 'row'}}>
+          <Image style={styles.ItemImg} source={{uri: item.img}} />
+          <Text numberOfLines={4} style={styles.ItemText}>
+            {item.text}
+          </Text>
+        </View>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <TouchableOpacity style={styles.ItemCopy} onPress={copyQuote}>
+            <Icon name="content-copy" size={30} color={Colors.white} />
+          </TouchableOpacity>
+        </View>
       </View>
     );
   };
@@ -19,7 +38,7 @@ function QuoteListTab() {
   };
   useEffect(() => {
     Axios.get(
-      'https://github.com/kikusourabh/StaterApp/blob/home-flatlist/Quotes/QuotesList.json',
+      'https://raw.githubusercontent.com/kikusourabh/QuotesStore/master/Quotes/QuotesList.json',
     )
       .then((res) => setData(res.data))
       .catch((err) => {
@@ -41,16 +60,26 @@ function QuoteListTab() {
 
 const styles = StyleSheet.create({
   Item: {
-    height: 56,
+    height: 100,
     backgroundColor: Colors.primaryLightColor,
+    flexDirection: 'row',
   },
   ItemSeparator: {
     height: 8,
     backgroundColor: Colors.secondaryColor,
   },
   ItemImg: {
-    height: 56,
-    width: 56,
+    height: 100,
+    width: 100,
   },
+  ItemText: {
+    color: Colors.primaryTextColor,
+    marginStart: 16,
+    marginEnd: 8,
+    width: 200,
+    fontSize: 18,
+    alignSelf: 'center',
+  },
+  ItemCopy: {},
 });
 export default QuoteListTab;
